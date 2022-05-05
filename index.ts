b4da87927754ebf91f7b69b8d5da2deae68b1c5d
rahul802515 <rahulkothari215@gmail.com>
2022-05-05 19:07:19 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -6,16 +6,45 @@ interface Car {
   color: string;
 }
 
+interface Scrap {
+  brand: string;
+  yearOfRelease: number;
+}
+
 import { faker } from '@faker-js/faker';
+import { Observable, filter } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 let car = (): Car => {
   return {
     name: faker.vehicle.vehicle(),
     model: faker.vehicle.model(),
-    yearOfRelease: faker.datatype.number({ min: 1990, max: 2022 }),
+    yearOfRelease: faker.datatype.number({ min: 1000, max: 5000 }),
     brand: faker.vehicle.manufacturer(),
     color: faker.vehicle.color(),
   };
 };
 
-console.log(car());
+let obs = new Observable((observer) => {
+  setInterval(() => {
+    observer.next(car());
+  }, 100);
+});
+
+// obs.subscribe((val) => {
+//   // console.log(val);
+// });
+
+let obs2 = obs.pipe(
+  filter<Car>((obj) => obj.color === 'black' && obj.yearOfRelease < 2000)
+);
+
+let obs3 = obs2.pipe(
+  map((obj) => {
+    return { brand: obj.brand, yearOfRelease: obj.yearOfRelease };
+  })
+);
+
+obs3.subscribe((val) => {
+  console.log(val);
+});
